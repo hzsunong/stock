df94e8dbc791cefa1707708435fe8ed5c78be2e8
Javen <w@juyii.com>
2017-09-18 15:26:32 +0800

    stock init

---
+++ src/instock/TestService.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Date: 2017/9/18
+ * Time: 下午3:24
+ */
+namespace SuNong\Stock\InStock;
+
+class TestService{
+
+    public function hello_earth(){
+        echo "世界上最帅的人是谁?";
+        echo PHP_EOL;
+        echo "当然是林甲文啦";
+    }
+}
